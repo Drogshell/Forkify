8ad68637142b36e2c22906ea3b56e6446dbe45a5
Drogshell <89346155+Drogshell@users.noreply.github.com>
2024-12-05 12:54:35 +1100

    Search results & errors
    - You can now search for recipes
    - Added a proper way to handle errors on the UI itself and not just through the console.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,31 +1,51 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import * as model from './model.js'
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
 import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultsView from './views/resultsView';
 
 // Custom API created by Jonas
 // https://forkify-api.herokuapp.com/v2
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+	module.hot.accept();
+}
 
 const controlRecipes = async function () {
-    try {
-        const id = window.location.hash.slice(1);
-        if (!id) return;
+	try {
+		const id = window.location.hash.slice(1);
+		if (!id) return;
 
-        //Loading recipe
-        recipeView.renderSpinner();
-        await model.loadRecipe(id);
-        //Rendering recipe
-        recipeView.render(model.state.recipe);
+		//Loading recipe
+		recipeView.renderSpinner();
+		await model.loadRecipe(id);
+		//Rendering recipe
+		recipeView.render(model.state.recipe);
+	} catch (err) {
+		recipeView.renderError();
+	}
+};
 
-    } catch (err) {
-        alert(err);
-    }
-}
+const controlSearchResults = async function () {
+	try {
+		resultsView.renderSpinner();
+		const query = searchView.getQuery();
 
-const init = function (){
-    recipeView.addHandlerRender(controlRecipes)
-}
+		if (!query) return;
+
+		await model.loadSearchResults(query);
+
+		console.log(model.state.search.results);
+		resultsView.render(model.state.search.results);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const init = function () {
+	recipeView.addHandlerRender(controlRecipes);
+	searchView.addHandlerSearch(controlSearchResults);
+};
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,27 +1,48 @@
-import {async} from "regenerator-runtime";
-import {API_URL} from "./config";
-import {getJSON} from "./helpers";
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
-    recipe: {},
+	recipe: {},
+	search: {
+		query: '',
+		results: [],
+	},
 };
 
 export const loadRecipe = async function (id) {
-    try {
-        const data = await getJSON(`${API_URL}/${id}`);
-        const {recipe} = data.data;
+	try {
+		const data = await getJSON(`${API_URL}${id}`);
 
-        state.recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            cookingTime: recipe.cooking_time,
-            ingredients: recipe.ingredients,
-        }
-    } catch (error) {
-        alert(error);
-    }
-}
+		const { recipe } = data.data;
+		state.recipe = {
+			id: recipe.id,
+			title: recipe.title,
+			publisher: recipe.publisher,
+			sourceUrl: recipe.source_url,
+			image: recipe.image_url,
+			servings: recipe.servings,
+			cookingTime: recipe.cooking_time,
+			ingredients: recipe.ingredients,
+		};
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const loadSearchResults = async function (query) {
+	try {
+		const data = await getJSON(`${API_URL}?search=${query}`);
+
+		state.search.results = data.data.recipe.map(recipe => {
+			return {
+				id: recipe.id,
+				title: recipe.title,
+				publisher: recipe.publisher,
+				image: recipe.image_url,
+			};
+		});
+	} catch (error) {
+		throw error;
+	}
+};
